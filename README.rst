42546cf3085948224523c0a3b0858a5ed4a5ae20
Wilmer Martinez <wilmermorelmartinez.gmail.com>
2021-06-10 20:36:09 -0400

    Versión 1.3
    Nuevos íconos de Bootstrap.

--- README.rst
+++ README.rst
@@ -1,24 +1,6 @@
 =====
-Unolet Utils
+unoletutils
 =====
 
-Unolet Utils es una aplicación Django con módulos, funciones, objetos, etc. 
-útiles para cualquier proyecto con Django.
-
-La documentación detallada se encuentra en el directorio "docs".
-
-Inicio rápido
------------
-
-1. Agregue "unoletutils" a su configuración INSTALLED_APPS de esta manera:
-
-     INSTALLED_APPS = (
-         ...
-         'unoletutils',
-     )
-
-2. Incluya la URLconf de unoletutils en su proyecto urls.py así:
-
-     url (r'^unoletutils/', include('unoletutils.urls')),
-
-3. Ejecute `python manage.py migrate` para crear los modelos de unoletutils.
+Django app with utils for Unolet.
+https://www.unolet.com/development/unoletutils/
--- setup.py
+++ setup.py
@@ -1,32 +1,20 @@
 import os
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
-
 # allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+#os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-unoletutils',
-    version='0.1',
-    packages=['unoletutils'],
+    name = 'django-unoletutils',
+    version = '1.3',
+    packages = ['unoletutils'],
     include_package_data=True,
-    license='BSD License',  # example license
-    description='Una app Django con utilidades para el proyecto.',
-    long_description=README,
+    license = 'BSD License',  # example license
+    description = 'Django app with utils for Unolet',
+    long_description = 'Django app with utils for Unolet. See more: https://www.unolet.com/development/unoletutils/',
     url='http://www.unolet.com/development/unoletutils/',
-    author='UNOLET',
-    author_email='soporte@unolet.com',
-    classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License', # example license
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        # Replace these appropriately if you are stuck on Python 2.
-        'Programming Language :: Python :: 3',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    ],
+    download_url = 'https://github.com/wilmerm/unoletutils.git',
+    author = 'Wilmer Martinez',
+    author_email = 'wilmermorelmartinez@gmail.com',
+    classifiers=[],
 )
