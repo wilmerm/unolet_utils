96e2f515ad22ea617253280dc5b5c31eb5ebfaeb
Wilmer Martinez <wilmermorelmartinez@gmail.com>
2021-07-19 23:52:48 -0400

    Performance

--- setup.py
+++ setup.py
@@ -1,20 +1,50 @@
-import os
-from setuptools import setup
+import pathlib
 
-# allow setup.py to be run from any path
-#os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+from setuptools import find_packages, setup
 
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+
+# This call to setup() does all the work
 setup(
-    name = 'django-unoletutils',
-    version = '1.5.1',
-    packages = ['unoletutils'],
+    name="django-unoletutils",
+    zip_safe=False,
+    version="1.5.2",
+    description="Django app with utils for Unolet",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="http://www.unolet.com/development/#django-unoletutils",
+    download_url = 'https://github.com/wilmerm/django-unoletutils.git',
+    author="Wilmer Martinez",
+    author_email="wilmermorelmartinez@gmail.com",
+    license="BSD-3-Clause",
+    packages=["unoletutils"], #find_packages(where="src"), # ["tojson"]
+    #package_dir={"": "src"},
     include_package_data=True,
-    license = 'BSD License',  # example license
-    description = 'Django app with utils for Unolet',
-    long_description = 'Django app with utils for Unolet. See more: https://www.unolet.com/development/unoletutils/',
-    url='http://www.unolet.com/development/unoletutils/',
-    download_url = 'https://github.com/wilmerm/unoletutils.git',
-    author = 'Wilmer Martinez',
-    author_email = 'wilmermorelmartinez@gmail.com',
-    classifiers=[],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "Django>=2.2",
+    ],
 )
